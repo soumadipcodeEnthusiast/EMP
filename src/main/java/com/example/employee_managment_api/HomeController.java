a48b49e8feb3527f9d9fbf8ddd097655ef7d47c5
soumadip <soum23@gmail.com>
2026-06-30 00:25:36 +0530

    files

---
+++ src/main/java/com/example/employee_managment_api/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.employee_managment_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee API is running successfully!";
+    }
+}
---
+++ src/test/java/com/example/employee_managment_api/EmployeeManagmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_managment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
